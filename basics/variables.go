f3a95fe59c822e2fbcfee827fd865ac412aa7166
Shriya Kalyan <shriyakkx@gmail.com>
2022-12-04 15:04:51 +0800

    Create variables.go

---
+++ basics/variables.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+var c, python, java bool
+
+func main() {
+	var i int
+	fmt.Println(i, c, python, java)
+}
+
+//variable can be both package as well as integer/string
